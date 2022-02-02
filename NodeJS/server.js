6bf410a0fde9c9a9e50862ea79ad28d30cb2ebe0
Shammo2 <samhammond2017@gmail.com>
2022-02-02 15:16:41 -0800

    angular and node

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,34 +1,14 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const io = require('socket.io');
-const { SocketAddress } = require('net');
-const PORT = 3000 || process.env.PORT;
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+app.get('/', (req, res) => res.send('hello!'));
 
-//set static folder 
-app.use(express.static(path.join(__dirname, '../frontend')));
-
-
-// run when client connects
-io.on('connection', socketio => {
-    Console.log('New WS Connection...');
-    socket.emit('message', 'welcome to GameParty');
-    //when a user connects 
-    socket.broadcast.emit('message','New Player has joined');
-    //when user disconnects
-    socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
-    });
-    Socket.on('chatmessage', (msg) => {
-      io.emit('message', message)
-    })
+io.on('connection', (socket) => {
+    console.log('a user connected');
 });
 
-server.listen(PORT, () => console.log('Server running on port',PORT));
-
-
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
 
